ec2422f81849f35e58805925232830963a0ff967
meganzz <43583631+meganzz@users.noreply.github.com>
2021-11-09 13:55:10 -0800

    ldpc

--- Gibbs-Sampling.py
+++ Gibbs-Sampling.py
@@ -44,7 +44,7 @@ class GibbsSampler():
             #print(p_v)
             v_new[i] = int(random.uniform(self.low, self.high) < p_v)
             if not self.binary:
-                v_new[i] = 1-2*v_new[i]
+                v_new[i] = 2*v_new[i]-1
         return v_new
     def run_sampling(self, v_init=None, visible_bits=None):
         if visible_bits is None:
@@ -59,8 +59,9 @@ class GibbsSampler():
             v = self.sample(v)
             if not self.binary:
                 dist_lst.append((1-v[:visible_bits])//2)
-            dist_lst.append(v[:visible_bits])
-        return dist_lst
+            else:
+                dist_lst.append(v[:visible_bits])
+        return dist_lst, v
 
 class BlockGibbsSampler(GibbsSampler):
     #For RBM
@@ -109,7 +110,6 @@ plot_graphs(and_dist_lst, "03", "AND: No Clamp", "ABC")
 
 """
 #This is for AND with general boltzmann machine:
-#weights and biases obtained from Wikipedia example
 W = np.array([
     [0, 0, 0, -9, -12, 4],
     [0, 0, 0, -9, 4, -12],
@@ -121,9 +121,71 @@ W = np.array([
 ])
 b = np.array([6, 6, 4, 4, 6, 6])
 and_sampler = GibbsSampler(W, b, 50, 1000000)
-and_dist_lst = and_sampler.run_sampling(visible_bits=3)
+and_dist_lst, v = and_sampler.run_sampling(visible_bits=3)
 #print(and_dist_lst)
 plot_graphs(and_dist_lst, "03", "AND: No Clamp", "ABC")
 """
 
 #This is for LDPC
+#visible units are in the order of sigma, p, then a.
+#sigma - original spins of message, from 1 to n (total of n)
+#p - auxiliary spins that represent the product of spins for a given sigma
+#   from 1 to k for the parity bits and from 1 to N_k for the nonzero elements
+#   in the kth row of the parity-check matrix H
+#a - auxiliary spins for the penalty term from 1 to k for the parity bits and
+#   from 2 to N_k for the nonzero elements in the kth row of H
+#weights and biases obtained from Wikipedia example
+n = 6
+k = 3
+h_km = 0.5 #from paper
+h = 0.3 #from paper
+H = np.array([
+    [1, 1, 1, 1, 0, 0],
+    [0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0]
+])
+r = np.array([1, 0, 1, 0, 1, 1])
+
+H_1st_zeroed_out = np.array([
+    [0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 1, 0]
+])
+b_sigma = -r+0.5+0.5*h_km*(np.sum(H_1st_zeroed_out, axis=0))
+b_p = 0.5*h_km + np.array([0, 0.5*h_km, 0.5*h_km, 0.5*h, 0, 0.5*h_km, 0.5*h, 0, 0.5*h_km, 0.5*h])
+b_a = h_km*np.ones(7)
+b = np.concatenate([b_sigma, b_p, b_a])
+W = np.array([
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,-0.5*h_km,-0.5*h_km,0,0,0,0,0,0,0,0,h_km,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,-0.5*h_km,-0.5*h_km,0,0,0,0,0,0,0,0,h_km,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,-0.5*h_km,-0.5*h_km,-0.5*h_km,-0.5*h_km,0,-0.5*h_km,-0.5*h_km,0,0,0,h_km,h_km,0,h_km,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,-0.5*h_km,-0.5*h_km,0,0,0,0,0,0,h_km],
+    [0,0,0,0,0,0,0,0,0,0,0,0,-0.5*h_km,-0.5*h_km,0,0,0,0,0,0,h_km,0,0],
+
+    [0,-0.5*h_km,0,0,0,0,0,-0.5*h_km,0,0,0,0,0,0,0,0,h_km,0,0,0,0,0,0],
+    [0,-0.5*h_km,-0.5*h_km,0,0,0,-0.5*h_km,0,-0.5*h_km,0,0,0,0,0,0,0,h_km,h_km,0,0,0,0,0],
+    [0,0,-0.5*h_km,-0.5*h_km,0,0,0,-0.5*h_km,0,-0.5*h_km,0,0,0,0,0,0,0,h_km,h_km,0,0,0,0],
+    [0,0,0,-0.5*h_km,0,0,0,0,-0.5*h_km,0,0,0,0,0,0,0,0,0,h_km,0,0,0,0],
+
+    [0,0,0,-0.5*h_km,0,0,0,0,0,0,0,-0.5*h_km,0,0,0,0,0,0,0,h_km,0,0,0],
+    [0,0,0,-0.5*h_km,0,-0.5*h_km,0,0,0,0,-0.5*h_km,0,-0.5*h_km,0,0,0,0,0,0,h_km,h_km,0,0],
+    [0,0,0,0,0,-0.5*h_km,0,0,0,0,0,-0.5*h_km,0,0,0,0,0,0,0,0,h_km,0,0],
+
+    [0,0,0,-0.5*h_km,0,0,0,0,0,0,0,0,0,0,-0.5*h_km,0,0,0,0,0,h_km,0,0],
+    [0,0,0,-0.5*h_km,-0.5*h_km,0,0,0,0,0,0,0,0,-0.5*h_km,0,-0.5*h_km,0,0,0,0,h_km,h_km,0],
+    [0,0,0,0,-0.5*h_km,0,0,0,0,0,0,0,0,0,-0.5*h_km,0,0,0,0,0,0,0,h_km],
+
+    [0,h_km,0,0,0,0,h_km,h_km,0,0,0,0,0,0,0,0,-2,0,0,0,0,0,0],
+    [0,0,h_km,0,0,0,0,h_km,h_km,0,0,0,0,0,0,0,0,-2,0,0,0,0,0],
+    [0,0,0,h_km,0,0,0,0,h_km,h_km,0,0,0,0,0,0,0,0,-2,0,0,0,0],
+    [0,0,0,h_km,0,0,0,0,0,0,h_km,h_km,0,0,0,0,0,0,0,-2,0,0,0],
+    [0,0,0,0,0,h_km,0,0,0,0,0,h_km,h_km,0,0,0,0,0,0,0,-2,0,0],
+    [0,0,0,h_km,0,0,0,0,0,0,0,0,0,h_km,h_km,0,0,0,0,0,0,-2,0],
+    [0,0,0,0,h_km,0,0,0,0,0,0,0,0,0,h_km,h_km,0,0,0,0,0,0,-2]
+])
+ldpc_sampler = GibbsSampler(W, b, 100000, 1000000, binary=False)
+ldpc_dist_lst, v = ldpc_sampler.run_sampling(visible_bits=6)
+print(v)
+#print(and_dist_lst)
+plot_graphs(ldpc_dist_lst, "06", "LDPC: 101011", "Message")
